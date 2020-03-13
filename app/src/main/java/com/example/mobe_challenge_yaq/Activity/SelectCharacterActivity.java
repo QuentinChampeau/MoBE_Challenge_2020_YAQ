ca0b3fafc257b5984a89701d970a39ccbcb9a4e9
AlexisMat <mateo.alexis82@gmail.com>
2020-03-13 11:29:19 +0100

    Dev : set de l'image du robot

--- app/src/main/java/com/example/mobe_challenge_yaq/Activity/SelectCharacterActivity.java
+++ app/src/main/java/com/example/mobe_challenge_yaq/Activity/SelectCharacterActivity.java
@@ -1,16 +1,44 @@
 package com.example.mobe_challenge_yaq.Activity;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.mobe_challenge_yaq.Bean.Robot;
 import com.example.mobe_challenge_yaq.R;
 
 public class SelectCharacterActivity extends AppCompatActivity {
+
+    private Robot robot;
+
+    private GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_character);
+
+        robot = new Robot("Rosé", null, 0, 0);
+
+        gridLayout = findViewById(R.id.gridSelectCharacter);
+
+        final int childCount = gridLayout.getChildCount();
+
+        for (int i = 0; i < childCount; i++) {
+            final int index = i;
+            final ImageView character = (ImageView) gridLayout.getChildAt(i);
+
+            character.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    BitmapDrawable bitmapDrawable = (BitmapDrawable) character.getDrawable();
+                    robot.setBitmap(bitmapDrawable.getBitmap());
+                }
+            });
+        }
     }
 }
