23af493b101dd9f6692d91bf9572370fb9aa2aff
YanRevuz <revuzyan@gmail.com>
2020-03-13 08:31:42 +0100

    Add dossier activity

---
+++ app/src/main/java/com/example/mobe_challenge_yaq/Activity/SetUpJ1Activity.java
@@ -0,0 +1,16 @@
+package com.example.mobe_challenge_yaq.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobe_challenge_yaq.R;
+
+public class SetUpJ1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
