4050e0e03eb860a683013525573d14a2901c7388
Hearner <quentin_ch@hotmail.fr>
2020-03-13 11:21:20 +0100

    Noise detection service w/ thread

--- app/src/main/java/com/example/mobe_challenge_yaq/MainActivity.java
+++ app/src/main/java/com/example/mobe_challenge_yaq/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.mobe_challenge_yaq;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.mobe_challenge_yaq.Activity.SelectNameActivity;
+import com.example.mobe_challenge_yaq.Service.NoiseService;
 import com.example.mobe_challenge_yaq.Service.ShakeService;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int SHAKE_THRESHOLD = 800;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private Context context;
 
@@ -29,11 +28,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestRecordAudioPermission();
+
         context = this;
         Button playButton = (Button) findViewById(R.id.playButton);
 
         // TODO remove context, il est utilisé que pour afficher un toast
         shakeService = new ShakeService((SensorManager) getSystemService(Context.SENSOR_SERVICE), context);
+
+        NoiseService.start();
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,5 +67,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         shakeService.getSensorManager().unregisterListener(shakeService.getmSensorListener());
         super.onPause();
+    }
+
+    public void requestRecordAudioPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
     }
 }
---
+++ app/src/main/java/com/example/mobe_challenge_yaq/Service/NoiseService.java
@@ -0,0 +1,44 @@
+package com.example.mobe_challenge_yaq.Service;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+public final class NoiseService {
+    private static final int NOISEDETECT = 27000;
+
+    public static void start() {
+        new Thread(new Runnable() {
+            public void run() {
+                listening();
+            }
+        }).start();
+    }
+
+    private static boolean listening() {
+        int minSize = AudioRecord.getMinBufferSize(8000,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+        AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                minSize);
+
+        short[] buffer = new short[minSize];
+
+        audioRecord.startRecording();
+        while (true) {
+
+            audioRecord.read(buffer, 0, minSize);
+            for (short s : buffer) {
+                if (Math.abs(s) > NOISEDETECT) {
+                    int blow_value = Math.abs(s);
+                    System.out.println("Blow Value = " + blow_value);
+                    audioRecord.stop();
+
+                    // stop
+                    return true;
+                }
+            }
+        }
+    }
+}
