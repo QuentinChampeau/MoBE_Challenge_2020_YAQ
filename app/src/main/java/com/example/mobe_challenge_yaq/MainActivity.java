bd8c4c37b7b511d6ed25be0929ed9814484dd4f1
Hearner <quentin_ch@hotmail.fr>
2020-03-13 09:07:59 +0100

    Play and Select name screens set up

--- app/src/main/java/com/example/mobe_challenge_yaq/MainActivity.java
+++ app/src/main/java/com/example/mobe_challenge_yaq/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.mobe_challenge_yaq;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.mobe_challenge_yaq.Activity.SelectNameActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        Button playButton = (Button) findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectPlayerName();
+            }
+        });
+    }
+
+    private void goToSelectPlayerName() {
+        Intent intent = new Intent(context, SelectNameActivity.class);
+        changeActivity(intent);
+    }
+
+    private void changeActivity(Intent i) {
+        startActivity(i);
     }
 }
