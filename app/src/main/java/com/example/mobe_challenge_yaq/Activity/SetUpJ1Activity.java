668338a610e9a328ad572690c0f035d60411d627
Hearner <quentin_ch@hotmail.fr>
2020-03-13 15:54:54 +0100

    Shake reset correction

--- app/src/main/java/com/example/mobe_challenge_yaq/Activity/SetUpJ1Activity.java
+++ app/src/main/java/com/example/mobe_challenge_yaq/Activity/SetUpJ1Activity.java
@@ -1,21 +1,14 @@
 package com.example.mobe_challenge_yaq.Activity;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Context;
 import android.content.Intent;
-
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,27 +16,17 @@ import com.example.mobe_challenge_yaq.Bean.Robot;
 import com.example.mobe_challenge_yaq.R;
 import com.example.mobe_challenge_yaq.Service.ShakeService;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
 
 public class SetUpJ1Activity extends AppCompatActivity {
-
-    private final int Max_COLONNE = 3;
-
     public static Robot joueur1;
 
-    private static  GridLayout gridLayout;
-
+    private static GridLayout gridLayout;
 
     private Button buttonFinal;
 
     private SelectCharacterActivity selectCharacterActivity;
 
-    private static  Context context;
+    private static Context context;
 
     private int lastPos = 11;
 
@@ -56,18 +39,16 @@ public class SetUpJ1Activity extends AppCompatActivity {
         context = this;
         gridLayout = findViewById(R.id.j1Activity);
 
-
         ShakeService.Start((SensorManager) getSystemService(Context.SENSOR_SERVICE));
-
 
         buttonFinal = findViewById(R.id.goToFinalActivity);
         buttonFinal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(!joueur1.getDeplacement().contains(lastPos) || joueur1.getDeplacement().get(joueur1.getDeplacement().size()-1) != lastPos){
+                if (!joueur1.getDeplacement().contains(lastPos) || joueur1.getDeplacement().get(joueur1.getDeplacement().size() - 1) != lastPos) {
                     System.out.println("PAS POSSIBLE");
-                }else {
+                } else {
 
                     Intent intent = new Intent(context, FinalActivity.class);
                     startActivity(intent);
@@ -87,43 +68,43 @@ public class SetUpJ1Activity extends AppCompatActivity {
                     Boolean bas = false;
                     Boolean droite = false;
                     Boolean gauche = false;
-                    Integer indexGauche =0;
-                    Integer indexDroite =0;
-                    Integer indexHaut =0;
-                    Integer indexBas =0;
-                    Integer lastIndex = joueur1.getDeplacement().size()-1;
-                    if(joueur1.getDeplacement().contains(index)){
+                    Integer indexGauche = 0;
+                    Integer indexDroite = 0;
+                    Integer indexHaut = 0;
+                    Integer indexBas = 0;
+                    Integer lastIndex = joueur1.getDeplacement().size() - 1;
+                    if (joueur1.getDeplacement().contains(index)) {
                         System.out.println("DEJA");
-                    }else{
+                    } else {
 
-                        if(index % 3 !=0){
-                            if(joueur1.getDeplacement().contains(index-1)){
+                        if (index % 3 != 0) {
+                            if (joueur1.getDeplacement().contains(index - 1)) {
                                 gauche = true;
-                                indexGauche = index-1;
+                                indexGauche = index - 1;
                             }
                         }
                         if (index % 3 != 2) {
                             if (joueur1.getDeplacement().contains(index + 1)) {
                                 droite = true;
-                                indexDroite = index +1;
+                                indexDroite = index + 1;
                             }
                         }
                         if (index + 3 < 12) {
                             if (joueur1.getDeplacement().contains(index + 3)) {
                                 bas = true;
-                                indexBas = index+3;
+                                indexBas = index + 3;
                             }
                         }
                         if (index - 3 >= 0) {
                             if (joueur1.getDeplacement().contains(index - 3)) {
                                 haut = true;
-                                indexHaut = index-3;
+                                indexHaut = index - 3;
                             }
                         }
-                        if((gauche && joueur1.getDeplacement().get(lastIndex) == indexGauche)
+                        if ((gauche && joueur1.getDeplacement().get(lastIndex) == indexGauche)
                                 || (droite && joueur1.getDeplacement().get(lastIndex) == indexDroite)
                                 || (haut && joueur1.getDeplacement().get(lastIndex) == indexHaut)
-                                || (bas && joueur1.getDeplacement().get(lastIndex) == indexBas)){
+                                || (bas && joueur1.getDeplacement().get(lastIndex) == indexBas)) {
                             joueur1.addDeplacement(index);
                             container.setImageBitmap(selectCharacterActivity.getRobotBitmap());
                         }
@@ -133,19 +114,18 @@ public class SetUpJ1Activity extends AppCompatActivity {
         }
     }
 
-
-    public static void removeAll(){
+    public static void removeAll() {
         joueur1.getDeplacement().removeAll(joueur1.getDeplacement());
         final int childCount = gridLayout.getChildCount();
         for (int i = 0; i < childCount; i++) {
             final int index = i;
             final ImageView container = (ImageView) gridLayout.getChildAt(i);
             if (i == joueur1.getPositionDepart()) {
+                joueur1.addDeplacement(joueur1.getPositionDepart());
                 container.setImageDrawable(context.getResources().getDrawable(R.mipmap.ic_launcher));
-            }else{
+            } else {
                 container.setImageDrawable(context.getResources().getDrawable(R.mipmap.ic_launcher_round));
             }
-
         }
     }
 
