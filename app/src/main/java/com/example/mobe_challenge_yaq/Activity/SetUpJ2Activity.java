b24a7dc9414102f5ca86755146ab2ce134e33fd1
AlexisMat <mateo.alexis82@gmail.com>
2020-03-13 15:09:48 +0100

    Dev : workflow correct de l'application

--- app/src/main/java/com/example/mobe_challenge_yaq/Activity/SetUpJ2Activity.java
+++ app/src/main/java/com/example/mobe_challenge_yaq/Activity/SetUpJ2Activity.java
@@ -1,10 +1,12 @@
 package com.example.mobe_challenge_yaq.Activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -25,11 +27,20 @@ public class SetUpJ2Activity extends AppCompatActivity {
 
     private Context context;
 
+    private Button nextActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_j2);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nextActivity = findViewById(R.id.mybutton);
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,SelectCharacterActivity.class);
+                startActivity(intent);
+            }
+        });
         context = this;
         positionTrap = new ArrayList<>();
         gridLayout = findViewById(R.id.j2Activity);
