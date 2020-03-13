928d915eb48bc5765872f0d1d779fd044a276e4a
AlexisMat <mateo.alexis82@gmail.com>
2020-03-13 11:16:30 +0100

    Dev : Activity pour la sélection d'un robot et déplacement des images dans drawable

---
+++ app/src/main/java/com/example/mobe_challenge_yaq/Activity/SelectCharacterActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobe_challenge_yaq.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobe_challenge_yaq.R;
+
+public class SelectCharacterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_character);
+    }
+}
